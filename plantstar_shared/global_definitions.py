dce4bd907120935c7f7db1bee0308547ebe32a7a
Macainian <robh@syscon-intl.com>
2022-05-31 14:23:16 -0400

    Renamed and reformatted

--- plantstar_shared/global_definitions.py
+++ plantstar_shared/global_definitions.py
@@ -2,10 +2,12 @@ import inspect
 
 from plantstar_shared import syscon_json
 
+
 def convert_to_json(the_class):
     base_dictionary = dict((key, value) for key, value in inspect.getmembers(the_class()) if not key.startswith("__") and not inspect.isfunction(value))
 
     return syscon_json.dumps(base_dictionary, default=lambda _: "Not Serializeable")
+
 
 class StatusCodes:
     OK = 200
