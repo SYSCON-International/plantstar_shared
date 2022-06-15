87b329bc790d6c668f0fded014eb1728c52d39a3
adamhochberger <adamhochberger@gmail.com>
2022-06-15 16:56:10 -0400

    Add utils for byte-dictionary conversion

---
+++ plantstar_shared/convert_dictionary_to_bytes.py
@@ -0,0 +1,8 @@
+from plantstar_shared import syscon_json
+
+# Converts the output dictionary into a string through syscon_json.dumps, then encodes it into bytes so that it can be sent through the socket, and parsed later on
+def convert_dictionary_to_bytes(output_dictionary, encoding_format='ASCII'):
+    output_dictionary_as_string = syscon_json.dumps(output_dictionary)
+    output_dictionary_as_bytes = output_dictionary_as_string.encode(encoding_format)
+
+    return output_dictionary_as_bytes
