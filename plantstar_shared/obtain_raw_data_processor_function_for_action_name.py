b39eef032e6d4a11b105b126c4d58d8e0cc9f8fa
adamhochberger <adamhochberger@gmail.com>
2022-06-15 12:10:16 -0400

    Add util function for use in APU and DCM processes

---
+++ plantstar_shared/obtain_raw_data_processor_function_for_action_name.py
@@ -0,0 +1,18 @@
+from plantstar_shared.RawDataProcessorInterfaceActions import RawDataProcessorInterfaceActions
+
+
+def obtain_raw_data_processor_function_for_action_name(rawdataprocessor, action_name):
+    if action_name == RawDataProcessorInterfaceActions.GET_DRIVER_VERSION_STRING:
+        return rawdataprocessor.get_driver_version_string
+    elif action_name == RawDataProcessorInterfaceActions.GET_INPUT_SIGNAL_DICTIONARIES:
+        return rawdataprocessor.get_input_signal_dictionaries
+    elif action_name == RawDataProcessorInterfaceActions.GET_VERSION_STRING:
+        return rawdataprocessor.get_version_string
+    elif action_name == RawDataProcessorInterfaceActions.UPDATE_ANALOG_INPUTS:
+        return rawdataprocessor.update_analog_inputs
+    elif action_name == RawDataProcessorInterfaceActions.UPDATE_DIGITAL_INPUT_DEBOUNCE:
+        return rawdataprocessor.update_digital_input_debounce
+    elif action_name == RawDataProcessorInterfaceActions.UPLOAD_NEW_DRIVER_SETTINGS:
+        return rawdataprocessor.upload_new_driver_settings
+    else:
+        raise Exception
