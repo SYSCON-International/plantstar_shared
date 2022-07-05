0b32b2fb66c60bab6cceb7874fd9d206510849ef
adamhochberger <adamhochberger@gmail.com>
2022-07-05 14:50:38 -0400

    Add constant for machine connection tracking

--- plantstar_shared/RawDataProcessorInterfaceActions.py
+++ plantstar_shared/RawDataProcessorInterfaceActions.py
@@ -3,6 +3,7 @@ class RawDataProcessorInterfaceActions:
     GET_INPUT_SIGNAL_DICTIONARIES = "get_input_signal_dictionaries"
     GET_MOCK_RAW_DATA_PROCESSOR_STATUS = "get_mock_raw_data_processor_status"
     GET_VERSION_STRING = "get_version_string"
+    SEND_IS_ALIVE_MESSAGE = "send_is_alive_message"
     UPDATE_ANALOG_INPUTS = "update_analog_inputs"
     UPDATE_DIGITAL_INPUT_DEBOUNCE = "update_digital_input_debounce"
     UPLOAD_NEW_DRIVER_SETTINGS = "upload_new_driver_settings"
