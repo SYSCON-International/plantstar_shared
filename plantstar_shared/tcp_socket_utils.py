5185efbb48f5248b77cc4e3bc1274ddc444d095c
Joseph T Lyons <JosephTLyons@gmail.com>
2022-07-07 15:00:54 -0400

    Fix some local variable names and formatting

--- plantstar_shared/tcp_socket_utils.py
+++ plantstar_shared/tcp_socket_utils.py
@@ -17,6 +17,7 @@ def read_size_value_from_socket(*, remote_socket):
     message_length = struct.unpack('>I', size_struct)[0]
     return message_length
 
+
 def get_message_from_socket(*, remote_socket):
     message_length = read_size_value_from_socket(remote_socket=remote_socket)
 
@@ -32,15 +33,18 @@ def get_message_from_socket(*, remote_socket):
         data.extend(packet)
     return data
 
+
 def send_message_on_socket(*, remote_socket, dumpsable_object, encoding='ASCII'):
     encoded_message = convert_object_to_bytes(input_object=dumpsable_object, encoding_format=encoding)
     send_encoded_message_on_socket(remote_socket=remote_socket, encoded_message=encoded_message)
 
+
 def send_encoded_message_on_socket(*, remote_socket, encoded_message):
-    size_of_message = add_size_onto_string_and_return(encoded_message)
-    remote_socket.send(encoded_message)
+    encoded_message_with_size = add_size_onto_string_and_return(encoded_message)
+    remote_socket.send(encoded_message_with_size)
+
 
 def get_output_dictionary_from_socket(*, remote_socket):
     output_dictionary_from_interface_as_bytes = get_message_from_socket(remote_socket=remote_socket)
-    output_dictionary_from_interface = convert_bytes_to_object(output_from_interface_as_bytes)
+    output_dictionary_from_interface = convert_bytes_to_object(output_dictionary_from_interface_as_bytes)
     return output_dictionary_from_interface
