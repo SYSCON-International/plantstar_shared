e7958304629c1d9c0575813636df8a7cafd99221
Joseph T Lyons <JosephTLyons@gmail.com>
2022-07-11 21:20:28 -0400

    Add `raise_invalid_type_error` function

--- plantstar_shared/errors.py
+++ plantstar_shared/errors.py
@@ -4,3 +4,7 @@ class InvalidApiRequest(Exception):
 
 class SysconProgrammingError(Exception):
     pass
+
+
+def raise_invalid_type_error(the_type, invalid_type_supplied):
+    raise SysconProgrammingError(f"Invalid \"{the_type.__name__}\" type: \"{invalid_type_supplied}\"")
