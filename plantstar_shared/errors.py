f6ecfe0034fca88408547b8d33ca436a05f259c4
Joseph T Lyons <JosephTLyons@gmail.com>
2022-07-11 21:23:52 -0400

    Return error instead of raise

--- plantstar_shared/errors.py
+++ plantstar_shared/errors.py
@@ -6,5 +6,5 @@ class SysconProgrammingError(Exception):
     pass
 
 
-def raise_invalid_type_error(the_type, invalid_type_supplied):
-    raise SysconProgrammingError(f"Invalid \"{the_type.__name__}\" type: \"{invalid_type_supplied}\"")
+def get_invalid_type_error(the_type, invalid_type_supplied):
+    return SysconProgrammingError(f"Invalid \"{the_type.__name__}\" type: \"{invalid_type_supplied}\"")
