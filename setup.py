552a3808174a8a3a39a0eae356940c3fe844ce45
adamhochberger <adamhochberger@gmail.com>
2022-07-21 05:32:37 -0400

    Add msgpack to build

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     install_requires=[
         # Make sure to update the requirements.txt to match any changes made to this section
         "Django>=4.0.4",
+        "msgpack>=1.0.4",
         "psycopg2>=2.9.3",
         "python-dateutil>=2.8.2",
         "pytz>=2022.1",
