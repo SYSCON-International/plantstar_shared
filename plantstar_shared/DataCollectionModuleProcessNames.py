962187938dacc7f165d33cefa2de3762e905938b
Joseph T Lyons <JosephTLyons@gmail.com>
2022-07-28 02:14:16 -0400

    Remove mock-process-related code

--- plantstar_shared/DataCollectionModuleProcessNames.py
+++ plantstar_shared/DataCollectionModuleProcessNames.py
@@ -1,12 +1,7 @@
 from plantstar_shared.SysconType import SysconType
 
 
-# `None` tells the system to use the real status retrieved, where a `bool` value is used to signify that we want to mock the status of some process: `is_running_mock_status`
-# Local dev systems do not use this, as the API call is entirely skipped on the APU
-# The first group represents Live VM Systems
-# The second group represents Live (Real) Systems
-
 class DataCollectionModuleProcessNames(SysconType):
-    PROCESS_SPAWNER = ("Process Spawner", None, None)
-    RAW_DATA_PROCESSOR_INTERFACE = ("Raw Data Processor Interface", None, None)
-    RTL_RIO_3 = ("RTL RIO 3", True, None)
+    PROCESS_SPAWNER = ("Process Spawner", )
+    RAW_DATA_PROCESSOR_INTERFACE = ("Raw Data Processor Interface", )
+    RTL_RIO_3 = ("RTL RIO 3", )
