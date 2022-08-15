de77967ee089dd961e8d312388b7afb9105904d8
Joseph T Lyons <JosephTLyons@gmail.com>
2022-08-15 16:25:49 -0400

    v0.1.26

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file_handle:
 
 setuptools.setup(
     name="plantstar-shared",
-    version="0.1.25",
+    version="0.1.26",
     author="SYSCON International",
     author_email="dev@syscon-intl.com",
     description="Shared code used in plantstar_apu and plantstar_dcm",
