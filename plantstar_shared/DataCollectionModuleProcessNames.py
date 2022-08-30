481fe9effba4debd16a393da38f2ca7d8eac809c
Joseph T Lyons <JosephTLyons@gmail.com>
2022-08-30 12:28:37 -0400

    Add missing entries for DataCollectionModuleProcessNames

--- plantstar_shared/DataCollectionModuleProcessNames.py
+++ plantstar_shared/DataCollectionModuleProcessNames.py
@@ -2,6 +2,8 @@ from plantstar_shared.SysconType import SysconType
 
 
 class DataCollectionModuleProcessNames(SysconType):
+    DIGITAL_ANALOG_PROCESSOR_INTERFACE = ("Digital Analog Processor Interface", )
+    DIGITAL_ANALOG_TASKER_INTERFACE = ("Digital Analog Tasker Interface", )
     PROCESS_SPAWNER = ("Process Spawner", )
     RAW_DATA_PROCESSOR_INTERFACE = ("Raw Data Processor Interface", )
     RTL_RIO_3 = ("RTL RIO 3", )
