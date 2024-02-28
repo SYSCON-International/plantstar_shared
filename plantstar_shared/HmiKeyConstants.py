7ec3ca2a39c3a566d54b2d2f0484ef72e4e3528a
Joseph Waggy <joseph.waggy@gmail.com>
2024-02-28 14:25:22 -0500

    Adds HmiApiTypes and HmiKeyConstants.

---
+++ plantstar_shared/HmiKeyConstants.py
@@ -0,0 +1,9 @@
+class HmiKeyConstants:
+    APU_IP_ADDRESS = "apu_ip_address"
+    HMI_HOSTNAME = "hmi_hostname"
+    HMI_HOMEPAGE = "hmi_homepage"
+    HMI_IP_ADDRESS_TYPE = "hmi_ip_address_type"
+    HMI_IP_ADDRESS = "hmi_ip_address"
+    HMI_NETMASK = "hmi_netmask"
+    HMI_DEFAULT_GATEWAY = "hmi_default_gateway"
+    HMI_DNS = "hmi_dns"
