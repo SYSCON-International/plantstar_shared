2db17d3ee9c5fcd28a5358c58337e0d3e6ae2d73
adamhochberger <adamhochberger@gmail.com>
2024-04-05 12:45:10 -0400

    Change usages of functions to match new logic

--- plantstar_shared/tcp_socket_utils.py
+++ plantstar_shared/tcp_socket_utils.py
@@ -57,7 +57,7 @@ def get_bytes_from_socket(*, remote_socket, number_of_bytes_to_read=None, is_big
 
     if not number_of_bytes_to_read:
         if number_of_bytes_for_size_prefix:
-            number_of_bytes_to_read = read_size_value_from_socket(
+            number_of_bytes_to_read, size_bytes = read_size_value_from_socket(
                 remote_socket=remote_socket, is_big_endian=is_big_endian, number_of_bytes_for_size_prefix=number_of_bytes_for_size_prefix
             )
 
@@ -105,9 +105,8 @@ def get_object_from_socket(
     *, remote_socket, number_of_bytes_to_read=None, is_big_endian=True, number_of_bytes_for_size_prefix=SIZE_OF_UNSIGNED_INT_STRUCT, should_remove_prefix_size_from_read=False
 ):
     """Function that is used between the APU and DCM to send dictionaries """
-    object_from_interface_as_bytes = get_bytes_from_socket(
-        remote_socket=remote_socket, number_of_bytes_to_read=number_of_bytes_to_read, is_big_endian=is_big_endian, number_of_bytes_for_size_prefix=number_of_bytes_for_size_prefix,
-        should_remove_prefix_size_from_read=should_remove_prefix_size_from_read
+    object_from_interface_as_bytes, _ = get_bytes_from_socket(
+        remote_socket=remote_socket, number_of_bytes_to_read=number_of_bytes_to_read, is_big_endian=is_big_endian, number_of_bytes_for_size_prefix=number_of_bytes_for_size_prefix
     )
     object_from_interface = convert_bytes_to_object(object_from_interface_as_bytes)
     return object_from_interface
