ded338a03c4d00a43ed1f680d55adee4ef55094e
Harrison Long <hclong.1999@gmail.com>
2025-04-16 10:23:24 -0400

    Update django package to 4.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     ],
     install_requires=[
         # Make sure to update the requirements.txt to match any changes made to this section
-        "Django>=4.1.5",
+        "Django>=4.2.0",
         "msgpack>=1.0.4",
         "psycopg2>=2.9.9",
         "python-dateutil>=2.8.2",
